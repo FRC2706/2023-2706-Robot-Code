28fd7e66d806c2455685b7f28e7bcfb2bd23e9a9
Jdiep7 <72757395+Jdiep7@users.noreply.github.com>
2023-01-14 11:49:41 -0500

    Jami mini swerve (#10)
    * Unfinished, swerve summer config values added.
    
    * Changed AHRS to Pigeon imu and Lamprey to CanCoder
    
    * gyro changed to m_pigeon, auto and commands added
    
    * Updates
    
    * Merging Pigeon an ids
    
    * Added library from team 3512
    
    Team 3512 wrote some helpful objects for swerve stuff.
    
    Sets proper status frames for Sparkmaxs and cancoder.
    
    Prepares a CanCoder configuration for mk4 swerve modules.
    
    Optimizes the steering angle for sparkmaxs (which we are already doing but this cleans it up into an organized and commented object.)
    
    * Config file swerve constants reorganized
    
    * Swerve Teleop updated
    
    * Updates 1
    
    * Swerve Updates
    
    * Small Touchups
    
    * Added swerve test states for 90 deg, and 1.5 speed
    
    * Swerve Updates
    
    * Added SyncSteerEncoders command
    
    Made a command that checks if the neo integrated encoder was properly reset to the cancoder+offset value at bootup. If it was not properly set, it will try to set it every few seconds until it is set properly.
    
    * Added ErrorCheck.java
    
    ErrorCheck has functions that can be imported as static and can catch any REVLibError's or ErrorCode's that return those codes (hardware devices).
    
    * Fix problem caused by BurnFlash
    
    BurnFlash needs to be called after doing passing all of the settings to the sparkmaxs. It was casuing issues when it setting PID values right after calling burnFlash.
    
    * Fix comparing doubles
    
    The range needs to be accurate enough that were are happy with the value if the module happens to be pointing forward when booted up. It's now checking to make sure the cancoder and neo encoder match to within 0.1 degrees.
    
    * Added AutoSelector and Pathweaver paths
    
    * Reduce Speed on Paths
    
    Doing some testing on a zoom call with some software people.
    
    Im reducing the speed so I dont forget later and have it break something...
    
    * Added field2d
    
    * Small PathWeaver Changes
    
    * Error Check in swerve module added
    
    * Tested Auto routines
    
    Changed the paths a bit + small tuning changes + speed up the paths.
    
    * Slew Rate Limiting added to code
    
    * Current Limiter Change
    
    Co-authored-by: ErikCald <erikcaell@gmail.com>
    Co-authored-by: Erik <43829793+ErikCald@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/SwerveTeleop.java
@@ -0,0 +1,74 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.config.Config;
+import frc.robot.config.Config.Swerve;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class SwerveTeleop extends CommandBase {
+    private Joystick driverStick;
+    /*private SlewRateLimiter transLimiter = new SlewRateLimiter(3.0);
+    private SlewRateLimiter strafeLimiter = new SlewRateLimiter(3.0);
+    private SlewRateLimiter rotationLimiter = new SlewRateLimiter(3.0);*/
+    
+
+    /** Creates a new DriveCommand. */
+    public SwerveTeleop(Joystick driverStick) {
+        this.driverStick = driverStick;
+        addRequirements(SwerveSubsystem.getInstance());
+
+    } 
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        SwerveSubsystem.getInstance().resetLastAngles();
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        
+        double x = -1 * driverStick.getRawAxis(XboxController.Axis.kLeftY.value); 
+        double y = -1 * driverStick.getRawAxis(XboxController.Axis.kLeftX.value);
+        double rot = -1 * driverStick.getRawAxis(XboxController.Axis.kRightX.value);
+
+        x = MathUtil.applyDeadband(x, Config.DRIVER_JOYSTICK_DEADBAND);
+        y = MathUtil.applyDeadband(y, Config.DRIVER_JOYSTICK_DEADBAND);
+        rot = MathUtil.applyDeadband(rot, Config.DRIVER_JOYSTICK_DEADBAND);
+
+        /*x = transLimiter.calculate(x);
+        y = strafeLimiter.calculate(y);
+        rot = rotationLimiter.calculate(rot);*/
+    
+        
+        SwerveSubsystem.getInstance().drive(
+            x * Config.Swerve.teleopSpeed,
+            y * Config.Swerve.teleopSpeed,
+            rot * Config.Swerve.kMaxTeleopAngularSpeed,
+            true, true);
+    }
+
+
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
--- src/main/java/frc/robot/robotcontainers/MiniSwerveContainer.java
+++ src/main/java/frc/robot/robotcontainers/MiniSwerveContainer.java
@@ -4,11 +4,22 @@
 
 package frc.robot.robotcontainers;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Robot;
+import frc.robot.auto.AutoSelector;
+import frc.robot.commands.ModuleAngleFromJoystick;
+import frc.robot.commands.ResetGyro;
+import frc.robot.commands.SwerveTeleop;
+import frc.robot.subsystems.SwerveModule;
+import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,10 +29,14 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
  */
 public class MiniSwerveContainer extends RobotContainer{
 
+  AutoSelector m_autoSelector;
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public MiniSwerveContainer() {
     // Configure the button bindings
     configureButtonBindings();
+
+    m_autoSelector = new AutoSelector();
   }
 
   /**
@@ -30,7 +45,43 @@ public class MiniSwerveContainer extends RobotContainer{
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    Joystick driver = new Joystick(0);
+
+    SwerveSubsystem.getInstance().setDefaultCommand(new SwerveTeleop(driver));
+
+    new JoystickButton(driver, XboxController.Button.kStart.value).whenPressed(
+      new InstantCommand(()-> SwerveSubsystem.getInstance().updateModulesPID())
+    );
+
+    new JoystickButton(driver, XboxController.Button.kBack.value).whenPressed(new ResetGyro());
+
+    new JoystickButton(driver, XboxController.Button.kY.value).whenPressed(
+      new InstantCommand(()-> SwerveSubsystem.getInstance().resetEncodersFromCanCoder())
+    );
+    
+    SwerveModuleState state1 = new SwerveModuleState(0.3, Rotation2d.fromDegrees(0));
+    SwerveModuleState state2 = new SwerveModuleState(0.3, Rotation2d.fromDegrees(90));
+    SwerveModuleState state3 = new SwerveModuleState(0.5, Rotation2d.fromDegrees(0));
+    SwerveModuleState state4 = new SwerveModuleState(-0.5, Rotation2d.fromDegrees(0));
+
+
+    Command angleSetPoint1 = new RunCommand(() -> SwerveSubsystem.getInstance().setModuleStates(new SwerveModuleState[]{state1, state1, state1, state1}, true), SwerveSubsystem.getInstance());
+    new JoystickButton(driver, XboxController.Button.kB.value).whenHeld(angleSetPoint1).whenReleased(new InstantCommand(SwerveSubsystem.getInstance() :: stopMotors, SwerveSubsystem.getInstance()));
+
+    Command angleSetPoint2 = new RunCommand(() -> SwerveSubsystem.getInstance().setModuleStates(new SwerveModuleState[]{state2, state1, state1, state1}, true), SwerveSubsystem.getInstance());
+    new JoystickButton(driver, XboxController.Button.kX.value).whenHeld(angleSetPoint2).whenReleased(new InstantCommand(SwerveSubsystem.getInstance() :: stopMotors, SwerveSubsystem.getInstance()));
+
+    Command angleSetPoint3 = new RunCommand(() -> SwerveSubsystem.getInstance().setModuleStates(new SwerveModuleState[]{state2, state2, state2, state2}, true), SwerveSubsystem.getInstance());
+    new JoystickButton(driver, XboxController.Button.kA.value).whenHeld(angleSetPoint3).whenReleased(new InstantCommand(SwerveSubsystem.getInstance() :: stopMotors, SwerveSubsystem.getInstance()));
+
+    Command angleSetPoint4 = new RunCommand(() -> SwerveSubsystem.getInstance().setModuleStates(new SwerveModuleState[]{state3, state3, state3, state3}, true), SwerveSubsystem.getInstance());
+    new JoystickButton(driver, XboxController.Button.kRightBumper.value).whenHeld(angleSetPoint4).whenReleased(new InstantCommand(SwerveSubsystem.getInstance() :: stopMotors, SwerveSubsystem.getInstance()));
+
+    Command angleSetPoint5 = new RunCommand(() -> SwerveSubsystem.getInstance().setModuleStates(new SwerveModuleState[]{state4, state4, state4, state4}, true), SwerveSubsystem.getInstance());
+    new JoystickButton(driver, XboxController.Button.kLeftBumper.value).whenHeld(angleSetPoint5).whenReleased(new InstantCommand(SwerveSubsystem.getInstance() :: stopMotors, SwerveSubsystem.getInstance()));
+  }
+
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -39,6 +90,8 @@ public class MiniSwerveContainer extends RobotContainer{
    */
   @Override
   public Command getAutonomousCommand() {
-    return new InstantCommand();
+    return m_autoSelector.getAutoCommand();
   }
+
+
 }
