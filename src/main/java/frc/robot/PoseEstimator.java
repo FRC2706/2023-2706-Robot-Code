37dd4a9140e0a42fca6bacd76df7f861a7b143ce
Jdiep7 <jdiep3@ocdsb.ca>
2023-01-30 20:34:05 -0500

    Pose estimation update

---
+++ src/main/java/frc/robot/PoseEstimator.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.IntegerSubscriber;
+
+/** Add your docs here. */
+public class PoseEstimator {
+    public static DoubleSubscriber distance;
+    public static DoubleSubscriber yaw;
+    public static DoubleSubscriber imageTimestamp;
+    public static IntegerSubscriber aprilTagID;
+
+    public static void update(){
+        if(distance.get(-99) == -99){
+            return;
+        }
+        if(yaw.get(-99) == -99){
+            return;
+        }
+        if(imageTimestamp.get(-99) == -99){
+            return;
+        }
+        if(aprilTagID.get(-99) == -99){
+            return;
+        }
+
+
+    }
+}
