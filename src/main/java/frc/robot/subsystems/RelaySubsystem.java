1c62e2c77e7a71da9d654d2377918a16dfa8b094
ErikCald <erikcaell@gmail.com>
2023-02-07 11:31:40 -0500

    Fix RelaySubsystem for NT4

--- src/main/java/frc/robot/subsystems/RelaySubsystem.java
+++ src/main/java/frc/robot/subsystems/RelaySubsystem.java
@@ -4,12 +4,15 @@
 
 package frc.robot.subsystems;
 
-import java.util.logging.Logger;
+import java.util.ArrayList;
 
+import edu.wpi.first.networktables.BooleanEntry;
+import edu.wpi.first.networktables.BooleanTopic;
 //import edu.wpi.first.networktables.EntryListenerFlags;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.Relay.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -37,12 +40,7 @@ public class RelaySubsystem extends SubsystemBase {
         return single_instance; 
     }
 
-    /**
-     * Set up all the instance variables related to hardware
-     */
-    private final Relay m_relay_ringlightRearSmall; 
-    private final Relay m_relay_ringlightRearLarge;
-    private final Relay m_relay_ringlightFront;
+    private final ArrayList<NTRelay> m_relays;
 
     /**
      * Construct the instance of this class
@@ -51,16 +49,73 @@ public class RelaySubsystem extends SubsystemBase {
         /**
          * Set up hardware stuff
          */
+        m_relays = new ArrayList<NTRelay>();
         
-        m_relay_ringlightRearSmall = new Relay(Config.RELAY_RINGLIGHT_REAR_SMALL); 
-        m_relay_ringlightRearLarge = new Relay(Config.RELAY_RINGLIGHT_REAR_LARGE);
-        m_relay_ringlightFront = new Relay(Config.RELAY_RINGLIGHT_FRONT);
-
         // Setup relays to be controlled from network tables
-        addNetworkTableRelay(m_relay_ringlightRearSmall, "Ringlight_Rear_Small");
-        addNetworkTableRelay(m_relay_ringlightRearLarge, "Ringlight_Rear_Large");
-        addNetworkTableRelay(m_relay_ringlightFront, "Ringlight_Front");
+        addNTRelay(Config.RELAY_RINGLIGHT_REAR_SMALL, "Ringlight_Rear_Small");
+        addNTRelay(Config.RELAY_RINGLIGHT_REAR_LARGE, "Ringlight_Rear_Large");
+        addNTRelay(Config.RELAY_RINGLIGHT_FRONT, "Ringlight_Front");
 
+    }
+
+    private class NTRelay {
+        private int m_portNumber;
+        private Relay m_relay;
+        private BooleanEntry m_entryNT;
+        boolean isOn;
+
+        public NTRelay(int portNumber, String topicName) {
+            m_portNumber = portNumber;
+            m_relay = new Relay(portNumber);
+            
+            BooleanTopic topic = NetworkTableInstance.getDefault()
+                .getTable(Config.RELAY_NETWORKTABLE)
+                .getBooleanTopic(topicName);
+            topic.setPersistent(true);
+
+            
+            m_entryNT = topic.getEntry(isOn);
+
+            isOn = m_entryNT.get(false);
+            setRelay(isOn);
+        }
+
+        public void checkNT() {
+            boolean entryValue = m_entryNT.get();
+            if (entryValue != isOn) {
+                setRelay(entryValue);
+            }
+        }
+
+        public void setRelay(boolean turnOn) {
+            isOn = turnOn;
+            if (isOn) {
+                m_relay.set(Value.kOn);
+            } else {
+                m_relay.set(Value.kOff);
+            }
+            updateNT();
+        }
+
+        public void toggleRelay() {
+            isOn = !isOn;
+            setRelay(isOn);
+        }
+
+        private void updateNT() {
+            m_entryNT.accept(isOn);
+        }
+
+        public int getPortNumber() {
+            return m_portNumber;
+        }
+    }
+
+    @Override
+    public void periodic() {
+        for (int i = 0; i < m_relays.size(); i++) {
+            m_relays.get(i).checkNT();
+        }
     }
 
     /**
@@ -72,56 +127,27 @@ public class RelaySubsystem extends SubsystemBase {
      */
 
     /**
-     * Get relay object based on a port number
-     */
-    private Relay getRelay(int portNumber) {
-        switch (portNumber) {
-            case Config.RELAY_RINGLIGHT_REAR_SMALL:
-                return m_relay_ringlightRearSmall;
-
-            case Config.RELAY_RINGLIGHT_REAR_LARGE:
-                return m_relay_ringlightRearLarge;
-
-            case Config.RELAY_RINGLIGHT_FRONT:
-                return m_relay_ringlightFront;
-
-            default: {
-                System.out.println(String.format("Relay at requested port number %d does not exist in code", portNumber));
-                return null;
-            }
-        }
-    }
-
-    /**
      * Add a network table entry to change a relay
      */
-    private void addNetworkTableRelay(Relay relay, String networkTableString) {
-        // Get the relay table
-        NetworkTable relayTable = NetworkTableInstance.getDefault().getTable(Config.RELAY_NETWORKTABLE);
+    private void addNTRelay(int relayPortNumber, String networkTableString) {
+        m_relays.add(new NTRelay(relayPortNumber, networkTableString));
+     }
 
-        // Create the entry
-        NetworkTableEntry relayEntry = relayTable.getEntry(networkTableString);
+    /**
+     * Get relay object based on a port number
+     */
+    private NTRelay getRelay(int portNumber) {
+        for (int i = 0; i < m_relays.size(); i++) {
+            if (m_relays.get(i).getPortNumber() == portNumber) {
+                return m_relays.get(i);
+            }
+        }
 
-        // Force the type to be a boolean
-       // relayEntry.forceSetBoolean(false);
-        relayEntry.setBoolean(false);
-        relayEntry.setPersistent();
-
-        // Relay of by default
-        relay.set(Value.kOff);
+        DriverStation.reportError(
+            String.format("Relay at requested port number %d does not exist in code", portNumber),
+            true);
         
-        // Setup the entry listener
-        // relayTable.addEntryListener(networkTableString, (table, key, entry, value, flags) -> {
-        //     boolean relayBol = (Boolean) value.getValue();
-        //     Value relayValue;
-        //     if (relayBol) 
-        //         relayValue = Value.kOn;
-        //     else 
-        //         relayValue = Value.kOff;
-
-        //     relay.set(relayValue);
-        // }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate); 
-
+        return null;
     }
 
     /**
@@ -141,40 +167,32 @@ public class RelaySubsystem extends SubsystemBase {
      * Use the values set in config for portNumbers
      */
     public void toggleRelay(int portNumber) {
-        Relay relay = getRelay(portNumber);
+        NTRelay relay = getRelay(portNumber);
 
         // Check if getRelay returned null
         if (relay == null) {
-            System.out.println(String.format("Null relay on port number %d", portNumber));
+            DriverStation.reportError(
+                String.format("Null relay on port number %d", portNumber), 
+                true);
             return;
         }
 
-        // Check for value of relay and set it to the opposite one.
-        Value value = relay.get();
-        switch (value) {
-            case kOn: 
-                relay.set(Value.kOff);
-                return;
-
-            case kOff:
-                relay.set(Value.kOn);
-                return;
-            
-            // Handle cases for kForward & kReverse
-            default:
-                relay.set(Value.kOff);
-                System.out.println(String.format(
-                    "toggle relay method was used when the relay on port %d was had value %s which it should only be used for kOn or kOff",
-                    portNumber, value.name()));
-                return;
-        }
+        relay.toggleRelay();
     }
 
     /**
      * Set the value of the relay
      */
-    public void setRelay(int portNumber, Value value) {
-        Relay relay = getRelay(portNumber);
-        relay.set(value);
+    public void setRelay(int portNumber, boolean turnOn) {
+        NTRelay relay = getRelay(portNumber);
+
+        // Check if getRelay returned null
+        if (relay == null) {
+            DriverStation.reportError(
+                String.format("Null relay on port number %d", portNumber), 
+                true);
+            return;
+        }
+        relay.setRelay(turnOn);
     }
 }
