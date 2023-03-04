acf3022759166748aaac4e0995c50530236a64c5
Jdiep7 <jdiep3@ocdsb.ca>
2023-03-04 14:14:01 -0500

    added setpoints and buttons to CompRobotContainer

--- src/main/java/frc/robot/robotcontainers/CompRobotContainer.java
+++ src/main/java/frc/robot/robotcontainers/CompRobotContainer.java
@@ -4,13 +4,29 @@
 
 package frc.robot.robotcontainers;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.Robot;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Robot;
+import frc.robot.commands.AlignToTargetVision;
+import frc.robot.commands.ResetGyro;
+import frc.robot.commands.ResetGyroToNearest;
+import frc.robot.commands.ResetOdometry;
+import frc.robot.commands.RotateAngleXY;
+import frc.robot.commands.RotateXYSupplier;
+import frc.robot.commands.SwerveTeleop;
+import frc.robot.commands.TranslationCommand;
+import frc.robot.config.Config;
+import frc.robot.subsystems.RelaySubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -33,9 +49,67 @@ public class CompRobotContainer extends RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    Joystick driverStick = new Joystick(0);
-    Joystick controlStick = new Joystick(1);
- }
+    CommandXboxController driverStick = new CommandXboxController(0);
+    CommandXboxController controlStick = new CommandXboxController(1);
+    CommandXboxController testStick = new CommandXboxController(2);
+
+
+    SwerveModuleState state1 =  new SwerveModuleState(0, Rotation2d.fromDegrees(0));
+    SwerveModuleState state2 =  new SwerveModuleState(0, Rotation2d.fromDegrees(90));
+    SwerveModuleState state3 =  new SwerveModuleState(-1.5, Rotation2d.fromDegrees(0));
+    SwerveModuleState state4 =  new SwerveModuleState(1.5, Rotation2d.fromDegrees(0));
+
+
+    SwerveSubsystem.getInstance().setDefaultCommand(new SwerveTeleop(driverStick));
+
+    driverStick.start().onTrue(new ResetGyroToNearest());
+    driverStick.back().onTrue(new ResetGyro());
+    //driverStick.b().onTrue(new InstantCommand(()-> SwerveSubsystem.getInstance().resetEncodersFromCanCoder()));
+    
+    //driverStick.b().whileTrue(new RotateXYSupplier(driverStick, ()-> visionYaw.get()));
+    //driverStick.b().whileTrue(new ResetOdometry(new Pose2d()));
+    driverStick.b().onTrue(new ResetOdometry(new Pose2d()));
+
+    driverStick.y().whileTrue(new RotateAngleXY(driverStick, 0));
+    driverStick.a().whileTrue(new RotateAngleXY(driverStick, Math.PI));
+    
+    driverStick.x().whileTrue(new TranslationCommand(1, 1));
+    driverStick.leftTrigger().whileTrue(new RotateXYSupplier(driverStick,
+      NetworkTableInstance.getDefault().getTable("pipelineTape21").getDoubleTopic("YawToTarget").subscribe(-99)
+    ));
+
+    driverStick.rightTrigger().whileTrue(Commands.sequence(
+      new AlignToTargetVision(driverStick,
+        NetworkTableInstance.getDefault().getTable("pipelineTape21").getDoubleTopic("YawToTarget").subscribe(-99),
+        NetworkTableInstance.getDefault().getTable("pipelineTape21").getDoubleTopic("DistanceToTarget").subscribe(-99),
+        2.0,
+        0.3,
+        1.5,
+        1.5),
+      new AlignToTargetVision(driverStick,
+        NetworkTableInstance.getDefault().getTable("pipelineTape21").getDoubleTopic("YawToTarget").subscribe(-99),
+        NetworkTableInstance.getDefault().getTable("pipelineTape21").getDoubleTopic("DistanceToTarget").subscribe(-99),
+        1.5,
+        0.03,
+        1,
+        0.5)
+    ));
+
+    RelaySubsystem.getInstance().setRelay(Config.RELAY_RINGLIGHT_REAR_LARGE, true);
+
+    
+    Command angleSetPoint1 = new RunCommand(() -> SwerveSubsystem.getInstance().setModuleStates(new SwerveModuleState[]{state1, state1, state1, state1}, true), SwerveSubsystem.getInstance());
+    testStick.a().whileTrue(angleSetPoint1).whileFalse(new InstantCommand(SwerveSubsystem.getInstance()::stopMotors, SwerveSubsystem.getInstance()));
+
+    Command angleSetPoint2 = new RunCommand(() -> SwerveSubsystem.getInstance().setModuleStates(new SwerveModuleState[]{state2, state2, state2, state2}, true), SwerveSubsystem.getInstance());
+    testStick.b().whileTrue(angleSetPoint2).whileFalse(new InstantCommand(SwerveSubsystem.getInstance()::stopMotors, SwerveSubsystem.getInstance()));
+
+    Command speedSetPoint1 = new RunCommand(() -> SwerveSubsystem.getInstance().setModuleStates(new SwerveModuleState[]{state3, state3, state3, state3}, true), SwerveSubsystem.getInstance());
+    testStick.y().whileTrue(speedSetPoint1).whileFalse(new InstantCommand(SwerveSubsystem.getInstance()::stopMotors, SwerveSubsystem.getInstance()));
+    
+    Command speedSetPoint2 = new RunCommand(() -> SwerveSubsystem.getInstance().setModuleStates(new SwerveModuleState[]{state4, state4, state4, state4}, true), SwerveSubsystem.getInstance());
+    testStick.x().whileTrue(speedSetPoint2).whileFalse(new InstantCommand(SwerveSubsystem.getInstance()::stopMotors, SwerveSubsystem.getInstance()));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
