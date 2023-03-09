65220270212b6fcb70c6ede43d5f0312bb04ad65
edwardwang4340 <edwardwangthebest@gmail.com>
2023-03-09 11:42:09 -0500

    created armwaypoint class
    not entirely sure how to do the ranges here tho

---
+++ src/main/java/frc/robot/subsystems/ArmWaypoint.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+/** Add your docs here. */
+public class ArmWaypoint {
+
+    private double x;
+    private double low_x_range;
+    private double high_x_range;
+    private double z;
+    private double low_z_range;
+    private double high_z_range;
+
+    public ArmWaypoint(double x, double z) {
+        this.x = x;
+        this.z = z;
+
+        
+
+
+    }
+
+}
