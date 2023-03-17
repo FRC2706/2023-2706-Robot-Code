9b1b63770a97ee2239cab0448a88341c7857db5a
weizhang-2706 <59770328+weizhang-2706@users.noreply.github.com>
2023-03-17 16:30:50 -0400

    modified for Humber for auto routines (#85)
    * modified for Humber
    
    * fixed load file names
    
    * Removed extra comments
    
    * Fixed 6/7 id issue.
    
    ---------
    
    Co-authored-by: ErikCald <erikcaell@gmail.com>

--- src/main/java/frc/robot/auto/AutoSelector.java
+++ src/main/java/frc/robot/auto/AutoSelector.java
@@ -21,25 +21,25 @@ public class AutoSelector {
         //use FRC Labview Dashboard
         String[] autoList = {
         "Id0_null", 
-        "Id1_cube_0p5_top_charge_good",
+        "Id1_cube_0p5_top_charge",
         "Id2_cube_0p5_bottom_charge",
         "Id3_cube_0p5_middle_charge", 
         "Id4_cube_1p0_top",
         "Id5_cube_1p0_bottom",
-        "Id6_cone_0p5_top_charge",
-        "Id7_cone_0p5_middle1_charge",
-        "Id8_cone_0p5_middle2_charge",
-        "Id9_cone_0p5_bottom_charge",
-        "Id10_cone_1p0_top",
-        "Id11_cone_1p0_bottom",
-        "Id12_BK_cube_2p0_bottom",
-        "Id13_BK_cube_0p5_middle_charge",
-        "Id14_BK_cone_0p5_charge",
-        "Id15_place_pick_bottom2_charge_new",
-        "Id16_place_pick_place_pick_place_bottom2",
-        "Id17_place_pick_place_pick_place_bottom2_charge",
-        "Id18_place_pick_place_pick_place_bottom_new",
-        "Id19_cube_1p0_top_charge"
+        "Id6_cone_0p5_top_charge"
+        // "Id7_cone_0p5_middle1_charge",
+        // "Id8_cone_0p5_middle2_charge",
+        // "Id9_cone_0p5_bottom_charge",
+        // "Id10_cone_1p0_top",
+        // "Id11_cone_1p0_bottom",
+        // "Id12_BK_cube_2p0_bottom",
+        // "Id13_BK_cube_0p5_middle_charge",
+        // "Id14_BK_cone_0p5_charge",
+        // "Id15_place_pick_bottom2_charge_new",
+        // "Id16_place_pick_place_pick_place_bottom2",
+        // "Id17_place_pick_place_pick_place_bottom2_charge",
+        // "Id18_place_pick_place_pick_place_bottom_new",
+        // "Id19_cube_1p0_top_charge"
         };
         SmartDashboard.putStringArray("Auto List", autoList );
     }
@@ -62,7 +62,7 @@ public class AutoSelector {
             case "Id0_null":
                 autoId = 0;
                 break;
-            case "Id1_cube_0p5_top_charge_good":
+            case "Id1_cube_0p5_top_charge":
                 autoId = 1;
                 break;
             case "Id2_cube_0p5_bottom_charge":
@@ -77,48 +77,48 @@ public class AutoSelector {
             case "Id5_cube_1p0_bottom":
                 autoId = 5;
                 break;
-            case "Id6_cone_0p5_top_charge":
-                autoId = 6;
-                break;
-            case "Id7_cone_0p5_middle1_charge":
+            case "Id7_cone_0p5_top_charge":
                 autoId = 7;
                 break;
-            case "Id8_cone_0p5_middle2_charge":
-                autoId = 8;
-                break;
-            case "Id9_cone_0p5_bottom_charge": 
-                autoId = 9;
-                break;
-            case "Id10_cone_1p0_top": 
-                autoId = 10; 
-                break;
-            case "Id11_cone_1p0_bottom": 
-                autoId = 11;
-                break;
-            case "Id12_BK_cube_2p0_bottom": 
-                autoId = 12;
-                break;
-            case "Id13_BK_cube_0p5_middle_charge": 
-                autoId = 13;
-                break;
-            case "Id14_BK_cone_0p5_charge": 
-                autoId = 14;
-                break;
-            case "Id15_place_pick_bottom2_charge_new":
-                autoId = 15;
-                break;
-            case "Id16_place_pick_place_pick_place_bottom2":
-                autoId = 16;
-                break;
-            case "Id17_place_pick_place_pick_place_bottom2_charge":
-                autoId = 17;
-                break;
-            case "Id18_place_pick_place_pick_place_bottom_new":
-                autoId = 18;
-                break;
-            case "Id19_cube_1p0_top_charge":
-                autoId = 19;
-                break;
+            // case "Id7_cone_0p5_middle1_charge":
+            //     autoId = 7;
+            //     break;
+            // case "Id8_cone_0p5_middle2_charge":
+            //     autoId = 8;
+            //     break;
+            // case "Id9_cone_0p5_bottom_charge": 
+            //     autoId = 9;
+            //     break;
+            // case "Id10_cone_1p0_top": 
+            //     autoId = 10; 
+            //     break;
+            // case "Id11_cone_1p0_bottom": 
+            //     autoId = 11;
+            //     break;
+            // case "Id12_BK_cube_2p0_bottom": 
+            //     autoId = 12;
+            //     break;
+            // case "Id13_BK_cube_0p5_middle_charge": 
+            //     autoId = 13;
+            //     break;
+            // case "Id14_BK_cone_0p5_charge": 
+            //     autoId = 14;
+            //     break;
+            // case "Id15_place_pick_bottom2_charge_new":
+            //     autoId = 15;
+            //     break;
+            // case "Id16_place_pick_place_pick_place_bottom2":
+            //     autoId = 16;
+            //     break;
+            // case "Id17_place_pick_place_pick_place_bottom2_charge":
+            //     autoId = 17;
+            //     break;
+            // case "Id18_place_pick_place_pick_place_bottom_new":
+            //     autoId = 18;
+            //     break;
+            // case "Id19_cube_1p0_top_charge":
+            //     autoId = 19;
+            //     break;
             default:
                 autoId = getAnalogSelectorIndex();
                 break;
