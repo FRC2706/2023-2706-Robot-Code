7935e73f2577c6ece9b2af2a8cfc3ef637872485
weizhang-2706 <wei2001ca@yahoo.com>
2023-03-22 21:28:33 -0400

    Added a new charge command with pigeon.

--- src/main/java/frc/robot/commands/ChargeCommandPigeon.java
+++ src/main/java/frc/robot/commands/ChargeCommandPigeon.java
@@ -10,35 +10,38 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SwerveSubsystem;
 
-public class ChargeCommandRoll extends CommandBase {
+public class ChargeCommandPigeon extends CommandBase {
   Timer m_timer = new Timer();
-  ProfiledPIDController pidControlX;
-  double currentX;
-  double desiredX;
-  double deltaX;
 
   ProfiledPIDController pidControlTheta;
   double currentTheta;
   double desiredTheta;
 
-  double pitchValue;
-  double initPitchValue;
-
-  int state = 0;
+  double pigeonValue;
+  double initPigeonValue;
+  boolean bWideSide;
+  double flipXDirection;
 
   double PITCH_TOLERANCE = 10;
-  double TIME_FOR_REVERSING = 0.10;
-  /** Creates a new ChargeCommandRoll. */
-  public ChargeCommandRoll( double deltaX) {
-    pidControlX = new ProfiledPIDController(1.2, 0.0, 0.2, 
-          new Constraints(2, 1.1));
 
+  //@todo: tune these values
+  double TIME_FOR_REVERSING = 0.10;
+  double X_SPEED_STATE0 = 1.5;
+  double X_SPEED_STATE1 = 1.0;
+  double X_SPEED_STATE2 = 0.5;
+
+  int state = 0;
+  double flipXState0;
+  double flipXState1;
+  double flipXState2;
+
+  /** Creates a new ChargeCommand. */
+  public ChargeCommandPigeon( boolean bWideSide, double flipXDirection) {
     pidControlTheta = new ProfiledPIDController(5.0,0, 0.4,
           new Constraints(4*Math.PI, 8*Math.PI));
 
-    
-    
-    this.deltaX = deltaX;
+    this.bWideSide = bWideSide;
+    this.flipXDirection = flipXDirection;
     pidControlTheta.enableContinuousInput(-Math.PI, Math.PI);
 
     addRequirements(SwerveSubsystem.getInstance());
@@ -47,21 +50,32 @@ public class ChargeCommandRoll extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    currentX = SwerveSubsystem.getInstance().getPose().getX();
-    desiredX = currentX + deltaX;
 
     currentTheta = SwerveSubsystem.getInstance().getHeading().getRadians();
     //keep the current angle
     desiredTheta = currentTheta;    
 
     //reset current positions
-    pidControlX.reset(currentX);
     pidControlTheta.reset(currentTheta);
 
-    //get the initial roll value
-    initPitchValue = SwerveSubsystem.getInstance().getRoll();
+    if( bWideSide == true)
+    {
+      //use the roll value
+      initPigeonValue = SwerveSubsystem.getInstance().getRoll();
+
+    }
+    else
+    {
+      //use the pitch value
+      initPigeonValue = SwerveSubsystem.getInstance().getPitch();
+    }
 
     state = 0;
+
+    flipXState0 = flipXDirection;
+    flipXState1 = flipXDirection;
+    flipXState2 = - flipXDirection;
+
     m_timer.stop();
     m_timer.reset();
   }
@@ -69,33 +83,52 @@ public class ChargeCommandRoll extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    currentX = SwerveSubsystem.getInstance().getPose().getX();
-    double x = pidControlX.calculate(currentX, desiredX);
-
     currentTheta = SwerveSubsystem.getInstance().getHeading().getRadians();
     double theta = pidControlTheta.calculate(currentTheta, desiredTheta);
 
+    if( bWideSide == true )
+    {
+      pigeonValue = SwerveSubsystem.getInstance().getRoll();
+    }
+    else
+    {
+      pigeonValue = SwerveSubsystem.getInstance().getPitch();
+    }
+    
 
-    pitchValue = SwerveSubsystem.getInstance().getRoll();
-
-    if (state == 0 && Math.abs(initPitchValue - pitchValue) > PITCH_TOLERANCE +1) 
+    if (state == 0 && Math.abs(initPigeonValue - pigeonValue) > PITCH_TOLERANCE +1) 
     {
       state = 1;
     }
-    if (state == 1 && Math.abs(initPitchValue - SwerveSubsystem.getInstance().getRoll()) < PITCH_TOLERANCE) {
+    if (state == 1 && Math.abs(initPigeonValue - pigeonValue) < PITCH_TOLERANCE) 
+    {
       state = 2;
       m_timer.restart();
     }
 
-    if (state == 2) {
-      SwerveSubsystem.getInstance().drive(-1 * Math.copySign(0.5, deltaX), 0, 0, true, false);
-    } else {
-      SwerveSubsystem.getInstance().drive(x, 0, theta, true, false);
+    double xSpeed = 0.0;
+    double xFlip = 1.0;
+
+    switch (state)
+    {
+      case 0:
+        xSpeed = X_SPEED_STATE0;
+        xFlip = flipXState0;
+        break;
+      case 1:
+        xSpeed = X_SPEED_STATE1;
+        xFlip = flipXState1;
+        break;
+      case 2:
+        xSpeed = X_SPEED_STATE2;
+        xFlip = flipXState2;
+        break;
+      default:
+      break;
     }
 
-
-    //@todo: use the deltaRollValue to determine x.
-
+    SwerveSubsystem.getInstance().drive(xSpeed*xFlip, 0, theta, true, false);
+  
   }
 
   // Called once the command ends or is interrupted.
