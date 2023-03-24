aa6267726a483e2b25e8eb6aace8ab8794a3dfdf
ErikCald <erikcaell@gmail.com>
2023-03-24 04:13:17 -0400

    Added tuning comments to ChargeCommandPigeon

--- src/main/java/frc/robot/commands/ChargeCommandPigeon.java
+++ src/main/java/frc/robot/commands/ChargeCommandPigeon.java
@@ -10,6 +10,24 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SwerveSubsystem;
 
+/**
+ * ChargeCommand is a command that has 3 states which it cycles through one by one until it reaches the final state.
+ * 
+ * State0:
+ *  - It starts in state 0. 
+ *  - The condition to move to state 1 is when the gyro pitch/roll value goes above a certain value. 
+ *  - This condition means the robot has changed from driving along the floor to driving up the ramp of the charge station.
+ * 
+ * State1:
+ *  - It changes from state 1 to state 2 when the gyro pitch/roll value goes back below a certain value. 
+ *  - This means the robot has gone past the tipping point of the charging station and the charge station is almost balanced.
+ * 
+ * State2:
+ *  - State 2 only runs for a short period of time (~0.1 on March23) and it drives backwards slightly to get rid of all the robot's speed.
+ * 
+ * After the command ends, it should be in a SequentialCommandGroup to then schedule a ChargeStationLock command in order to lock the
+ *    wheels in a pattern that prevents it from rolling off the charging station if it's not balanced.
+ */
 public class ChargeCommandPigeon extends CommandBase {
   Timer m_timer = new Timer();
 
@@ -27,15 +45,61 @@ public class ChargeCommandPigeon extends CommandBase {
   boolean bWideSide;
   double flipXDirection;
 
+  // Erik: This 10 degrees was tested to work well with the old ChargeCommand and should still be good.
+  //           Only change this is it's not properly going through the states but preferably don't touch it.
   double PITCH_TOLERANCE = 10;
 
-  //@todo: tune these values
-  double TIME_FOR_REVERSING = 0.10;
-  double X_WIDE_LIMIT = 4.0;
+  // Erik: These are just safety factors to make sure the robot doesn't drive to the other side of the 
+  //          field if something goes wrong. 4.5 meters is perfect.
+  double X_WIDE_LIMIT = 4.5;
   double X_NARROW_LIMIT = 4.5;
-  double X_SPEED_STATE0 = 1.7; //2.0
+
+  /**
+   * Erik Tuning suggestions for ChargeCommandPigeon
+   * 
+   * Note:
+   * - You want to tune this command such that it stops in the MIDDLE of the charging station.
+   *      Even if it's balanced that's not good enough. It should be tuned so that it stops in
+   *      the middle.
+   * 
+   * Tips:
+   * - Special case: It's possible X_SPEED_STATE0 is too high if it never decelerates to X_SPEED_STATE1 
+   *        before reaching X_SPEED_STATE2. If this is true, decrease X_SPEED_STATE0.
+   * 
+   * - If it fails to tip the charging station, increase X_SPEED_STATE0 (but I expect this won't be needed)
+   * 
+   * 
+   * ~ If it's gone too far, do one of the following:
+   *    - Increase the "jump back", aka increase either TIME_FOR_REVERSING or X_SPEED_STATE2.
+   *    - Decrease X_SPEED_STATE1 to approach the center at a slower rate.
+   * 
+   * ~ If it's not gone far enough, do one of the following:
+   *    - Increase X_SPEED_STATE1 to climb up the ramp at a faster speed, making it go farther before the "jump back" happens.
+   *    - If you have a huge "jump back", reduce the "jump back". If you have a small "jump back" then leave it alone.
+   *      - Decrease either TIME_FOR_REVERSING or X_SPEED_STATE2 to reduce "jump back".
+   *    
+   *   
+   * There are 2 ways to approach tuning this command:
+   * ~ Method 1: Slow but consistent:
+   *    - Tune X_SPEED_STATE1 to slowly crawl up the ramp.
+   *    - Have a tiny jump back that does almost nothing (just gets rid of most the speed)
+   * 
+   * ~ Method 2: Save 1 to 3 seconds but a little chaotic
+   *    - Tune X_SPEED_STATE1 to drive up the ramp faster than a slow crawl
+   *    - Tune X_SPEED_STATE1 to be fast enough it ends up going slightly too far
+   *    - Tune the "jump back" actually jump back, getting rid of all speed from X_SPEED_STATE1 and
+   *          have it go back by 0.1 meters.
+   * 
+   * The best way will probably be mostly method 1 with a bit of method 2.
+   */
+
+  // Unit: seconds
+  double TIME_FOR_REVERSING = 0.10;
+
+  // Unit: meters per second
+  double X_SPEED_STATE0 = 1.7;
   double X_SPEED_STATE1 = 0.7;
-  double X_SPEED_STATE2 = 0.5;
+  double X_SPEED_STATE2 = -0.5; // Negative because it should "jump back" or just get rid of any velocity.
 
   int state = 0;
   double flipXState0;
@@ -84,7 +148,7 @@ public class ChargeCommandPigeon extends CommandBase {
 
     flipXState0 = flipXDirection;
     flipXState1 = flipXDirection;
-    flipXState2 = - flipXDirection;
+    flipXState2 = flipXDirection;
 
     m_timer.stop();
     m_timer.reset();
