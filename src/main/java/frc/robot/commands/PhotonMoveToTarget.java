3922fd4dd239748091f4b57e45d57755de81da76
Codingfor1 <Keira.sullivan28@gmail.com>
2023-11-08 20:57:53 -0500

    stuff

--- src/main/java/frc/robot/commands/PhotonMoveToTarget.java
+++ src/main/java/frc/robot/commands/PhotonMoveToTarget.java
@@ -10,7 +10,9 @@ import org.photonvision.*;
 import org.photonvision.targeting.TargetCorner;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -18,8 +20,9 @@ import frc.robot.subsystems.DiffTalonSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class PhotonMoveToTarget extends CommandBase {
-  double EXAMPLE_SIZE_HEIGHT = 25.808;
-  double EXAMPLE_DISTANCE = 2.000;
+  double EXAMPLE_SIZE_HEIGHT = 104.6;
+  double EXAMPLE_DISTANCE = 1.000;
+  Pose2d cameraOffset = new Pose2d(new Translation2d(0.3,0.3), Rotation2d.fromDegrees(0));
   //height of april = 1 foot 3 and 1/4 to bottom of black
   /** Creates a new ExampleCommand. */
   PhotonCamera camera1;
@@ -27,19 +30,20 @@ public class PhotonMoveToTarget extends CommandBase {
   PIDController yController;
   PIDController yawController;
   Translation2d setPoint;
-  boolean seen = false;
+  LinearFilter filterX = LinearFilter.movingAverage(5);
+  LinearFilter filterY = LinearFilter.movingAverage(5);
+
 
   public PhotonMoveToTarget() {
     camera1 = new PhotonCamera("OV9281");
-    xController = new PIDController(0.7, 0, 0);
-    yController = new PIDController(0.7, 0, 0);
+    xController = new PIDController(1.2, 0, 0);
+    yController = new PIDController(1.2, 0, 0);
     yawController = new PIDController(0.06, 0, 5);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    seen = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -50,24 +54,26 @@ public class PhotonMoveToTarget extends CommandBase {
     if (result.hasTargets()){
       List<TargetCorner> corners = result.getBestTarget().getDetectedCorners();
       double heightSize = (corners.get(0).y - corners.get(3).y + corners.get(1).y - corners.get(2).y)/2;
-      double range = heightSize/EXAMPLE_SIZE_HEIGHT*EXAMPLE_DISTANCE;
-      double yaw = -result.getBestTarget().getYaw() + odometryPose.getRotation().getDegrees();
-
-      double targetX = Math.cos(yaw)*range + odometryPose.getX()-1;
-      System.out.println("range: " + range + " cos: " + Math.cos(yaw) + " Odometry: " + odometryPose.getX());
-      double targetY = Math.sin(yaw)*range + odometryPose.getY();
-      if (seen == false)
-        setPoint = new Translation2d((targetX),(targetY));
-      if (seen) 
-        setPoint = new Translation2d((setPoint.getX()+targetX)/2,(setPoint.getY()+targetY)/2);
-      seen = true;
-      }
-      if (seen) {
+      System.out.println(heightSize);
+      double range = EXAMPLE_SIZE_HEIGHT*EXAMPLE_DISTANCE/heightSize;
+      System.out.println("range"+range);
+      Rotation2d yaw = Rotation2d.fromDegrees(-result.getBestTarget().getYaw()); //+ odometryPose.getRotation().getDegrees());
+      
+      Translation2d visionXY = new Translation2d(range, yaw);
+      Translation2d robotToTargetRELATIVE = cameraOffset.getTranslation().plus(visionXY);
+      Translation2d robotToTarget = robotToTargetRELATIVE.rotateBy(odometryPose.getRotation());
+      Translation2d feildToTarget = robotToTarget.plus(odometryPose.getTranslation());
+      System.out.println("robot to target "+ robotToTargetRELATIVE.toString());
+      System.out.println("odometry"+ odometryPose.toString());
+      
+      setPoint = new Translation2d(filterX.calculate(feildToTarget.getX())-1,filterY.calculate(feildToTarget.getY()));
+      System.out.println("set "+setPoint);
+    }
       double xSpeed = xController.calculate(odometryPose.getX(), setPoint.getX());
       double yspeed = yController.calculate(odometryPose.getY(), setPoint.getY());
       SwerveSubsystem.getInstance().drive(xSpeed, yspeed, 0, true, false);
-      }
-    }
+  }
+  
 
   // Called once the command ends or is interrupted.
   @Override
